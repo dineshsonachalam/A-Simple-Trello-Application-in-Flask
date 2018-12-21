ca5fb9bb46a69e806a67ec6424d48addc5febfc2
Dinesh <dineshsonachalam@gmail.com>
2018-12-22 01:09:53 +0530

    Added Dockerfile

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ app.register_blueprint(boards_blueprint)
 
 
 if __name__ == '__main__':
-    app_host = os.environ.get('APP_HOST') or 'localhost'
-    app_port = os.environ.get('APP_PORT') or 8026
+    app_host = os.environ.get('APP_HOST') or '0.0.0.0'
+    app_port = os.environ.get('APP_PORT') or 8002
     app.run(host=app_host, port=app_port, debug=True, threaded=True)
-    app.run(debug=True)
